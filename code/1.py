68792b3aa8ce54eb5d6490223fc1bd3d3f8cdc88
wangjie <1115794188@qq.com>
2019-11-01 21:41:39 +0800

    some change

--- code/1.py
+++ code/1.py
@@ -1,25 +1,19 @@
 from CornellDataset import CornellDataset
 import torch.utils.data as Data
 import numpy as np
-import torch
+import torch, torchvision
+import os.path
+import PIL.Image as Image
+import cv2
 
-# dataset = CornellDataset("D:\\Cornell_data\\01")
-# dataloader = Data.DataLoader(dataset=dataset)
-#
-#
-# data = []
-# image_path = "D:\\Cornell_data\\01\\image"
-# data_path = "D:\\Cornell_data\\01\\pos_label"
-# for image, pos_label in zip(os.scandir(image_path), os.scandir(data_path)):
-#     image_label = []
-#     for lines in open(pos_label):
-#         image_label.append(list(map(float, lines.split())))
-#     image_label = np.array(image_label)
-#     data.append([image, image_label])
-#
-# print(data[:1])
-a = np.array([[1, 2, 3, 4], [2, 3, 4, 1], [4, 5, 6, 7]])
-b = np.array([[0, 2, 1], [1, 0, 2], [2, 0, 1]])
-a = torch.from_numpy(a)
-b = torch.from_numpy(b)
-print(a[:, b.long()][torch.arange(3), torch.arange(3)])
+root = 'D:\\Cornell_data\\dataset'
+dataset = CornellDataset(root)
+dataloader = Data.DataLoader(dataset, batch_size=5)
+for data in dataloader:
+    img, label = data
+    print(img.shape)
+    print(label)
+
+
+
+
