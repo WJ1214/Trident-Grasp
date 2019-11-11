96a4ab7d0becffb5004edbfccd113e15905016c1
wangjie <1115794188@qq.com>
2019-11-11 21:06:25 +0800

    add some function test

--- 1.py
+++ 1.py
@@ -1,11 +1,26 @@
 from data.CornellDataset import CornellDataset
 import torch.utils.data as Data
 import torchvision.transforms as tvtsf
+import cv2
+import numpy as np
+import data.util as util
+from data.util import numpy_box2list
 
 root = 'D:/Cornell_data/dataset'
 dataset = CornellDataset(root)
-data = dataset.__getitem__(0)
-img, label = data
+dataloader = Data.DataLoader(dataset)
+for data in dataloader:
+    img, box, angle = data
+    print('image:', img)
+    print('box:', box)
+    print('angle:', angle)
+
+
+# for j in range(len(bbox)):
+#     for i in range(4):
+#         cv2.circle(img, tuple(bbox[j][i]), 3, (255, 0, 0), -1)
+#         cv2.imshow('1', img)
+#         cv2.waitKey(0)
 
 
 
