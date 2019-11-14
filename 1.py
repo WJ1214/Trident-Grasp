da114902d871b717c16a00a581bdb7efe13b0710
wangjie <1115794188@qq.com>
2019-11-14 13:48:47 +0800

    add some function test

--- 1.py
+++ 1.py
@@ -1,26 +1,30 @@
 from data.CornellDataset import CornellDataset
 import torch.utils.data as Data
-import torchvision.transforms as tvtsf
+import torchvision.transforms as transform
 import cv2
 import numpy as np
 import data.util as util
-from data.util import numpy_box2list
 
 root = 'D:/Cornell_data/dataset'
-dataset = CornellDataset(root)
-dataloader = Data.DataLoader(dataset)
-for data in dataloader:
+trans = util.RandomCenterCrop()
+dataset = CornellDataset(root, True, trans)
+dataset1 = CornellDataset(root, True)
+# data = dataset.__getitem__(0)
+# img, box, angle = data
+# data = dataset.__getitem__(0)
+# img, box, angle = data
+# cv2.imshow('1', img)
+# cv2.waitKey(0)
+
+dataloader = Data.DataLoader(dataset, 1)
+for i, data in enumerate(dataloader):
     img, box, angle = data
-    print('image:', img)
-    print('box:', box)
-    print('angle:', angle)
+    util.show_batch_image(img, box)
 
 
-# for j in range(len(bbox)):
-#     for i in range(4):
-#         cv2.circle(img, tuple(bbox[j][i]), 3, (255, 0, 0), -1)
-#         cv2.imshow('1', img)
-#         cv2.waitKey(0)
+
+
+
 
 
 
