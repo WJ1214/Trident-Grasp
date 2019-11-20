d6b8a3d6619a9412f07ddf712b0fdf050cccd57c
wangjie <1115794188@qq.com>
2019-11-20 16:21:38 +0800

    delete some test func and add anchor box

--- 1.py
+++ 1.py
@@ -7,36 +7,41 @@ import data.util as util
 import matplotlib.pyplot as plt
 from skimage import io, data, transform
 import math
+from Module.utils.bbox_tools import generate_base_anchor, enumerate_shifted_anchor
+import torch
 
+
+# 数据集测试
 root = 'D:/Cornell_data/dataset'
 trans = util.PreProcess()
 dataset = CornellDataset(root, True, trans)
-data = dataset.__getitem__(0)
-img, box, angle = data
-# cv2.imshow('1', img)
-# cv2.waitKey(0)
+# data = dataset.__getitem__(0)
+# img, box, angle = data
+# anchor_box, _ = generate_base_anchor()
+# util.show_bbox_image(img, anchor_box)
 
+
+# 预处理测试
+# dataloader = Data.DataLoader(dataset, 1)
+# for i, data in enumerate(dataloader):
+#     img, box, angle = data
+#     util.show_batch_image(img, box)
+
+
+# base_anchor测试
+anchor_box, _ = generate_base_anchor(32, 150)
+# x = torch.arange(0, 10 * 32, 32)
+# y = torch.arange(0, 10 * 32, 32)
+# x, y = np.meshgrid(x, y)
+# shift = np.stack((x.ravel(), y.ravel(), x.ravel(), y.ravel(), x.ravel(), y.ravel(), x.ravel(), y.ravel()), axis=1)
+# anchor_box = anchor_box[0] + shift
+anchor_box = enumerate_shifted_anchor(anchor_box, 32, 10, 10)
+anchor_box = np.array([anchor_box])
 dataloader = Data.DataLoader(dataset, 1)
 for i, data in enumerate(dataloader):
     img, box, angle = data
-    print('box:', box)
-    print('angle:', angle)
-    util.show_batch_image(img, box)
+    util.show_batch_image(img, anchor_box)
 
-# img_path = dataset.image[0]
-# img = io.imread(img_path)
-# img = img[80:400, 160:480, :]
-# point = np.array((110, 175))
-#
-# plt.imshow(img)
-# plt.scatter(point[0], point[1], color='b', marker='.')
-# plt.show()
-#
-# point1 = calculate_loc(point, img, 30)
-# img = transform.rotate(img, 30, False)
-# plt.imshow(img)
-# plt.scatter(point1[0], point1[1], color='b', marker='.')
-# plt.show()
 
 
 
