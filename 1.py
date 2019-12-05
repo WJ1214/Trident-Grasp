d9052df3468c226831555018e6a954f523edddf5
WJ1214 <51085092+WJ1214@users.noreply.github.com>
2019-12-05 14:49:07 +0800

    add nms test

--- 1.py
+++ 1.py
@@ -8,13 +8,12 @@ import data.util as util
 import math
 from Module.utils.bbox_tools import generate_base_anchor, enumerate_shifted_anchor
 import torch.nn as nn
-from Module.TestModule.testnet import resnet50
-from dcn.modules.modulated_deform_conv import ModulatedDeformConv
+from Module.nms.NMS import intersection, nms, cpu_nms, non_max_suppression
 
 
 # 数据集测试
 root = 'D:/Cornell_data/dataset'
-# trans = util.PreProcess()
+trans = util.PreProcess()
 # dataset = CornellDataset(root, True, trans)
 # data = dataset.__getitem__(0)
 # img, box, angle = data
@@ -36,9 +35,33 @@ root = 'D:/Cornell_data/dataset'
 # dataloader = Data.DataLoader(dataset, 1)
 # for i, data in enumerate(dataloader):
 #     img, box, angle = data
-#     util.show_batch_image(img, anchor_box)
+#     util.show_batch_image(img, box)
 
-net = resnet50()
+test_data = [[204, 102, 358, 102, 358, 250, 204, 250, 0.5],
+             [257, 118, 380, 118, 380, 250, 257, 250, 0.7],
+             [280, 135, 400, 135, 400, 250, 280, 250, 0.6],
+             [255, 118, 360, 118, 360, 235, 255, 235, 0.7]]
+dets = np.array([
+                [204, 102, 358, 250, 0.5],
+                [257, 118, 380, 250, 0.7],
+                [280, 135, 400, 250, 0.6],
+                [255, 118, 360, 235, 0.7]])
+
+data2 = [[10, 10, 15, 10, 15, 20, 10, 20, 0.8],
+         [12, 13, 17, 13, 17, 23, 12, 23, 0.7],
+         [50, 20, 60, 20, 60, 70, 50, 70, 0.6]]
+test_data2 = [[10, 10, 15, 20, 0.8],
+              [12, 13, 17, 23, 0.7],
+              [50, 20, 60, 70, 0.6]]
+data2 = np.array(data2)
+test_data2 = np.array(test_data2)
+
+test_data = np.array(test_data)
+rotate_keep = nms(data2, 0.8)
+keep = cpu_nms(test_data2, 0.8)
+print(rotate_keep)
+print(keep)
+
 
 
 
